725c45b389ba85a1e1693c0d23a6e0c52d814d2d
tsandovalr <63921337+tsandovalr@users.noreply.github.com>
2021-06-03 22:37:11 -0400

    Add files via upload

--- repaso_web_shop/server.js
+++ repaso_web_shop/server.js
@@ -3,6 +3,9 @@ const app = require('./app');
 require('dotenv').config();
 
 const port = process.env.port || 3000;
+console.log('Connected on port:'+port);
+
+
 
 const server= http.createServer(app);
 
